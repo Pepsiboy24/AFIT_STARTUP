3f15052696152482372c98d7f7cac6b602781f65
Pepsiboy24 <amramgadzama7@gmail.com>
2026-08-20 07:05:07 +0100

    Added Backend changes

--- .next/dev/static/chunks/src_app_search_page_tsx_8e9f1618._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_651c9350._.js"
-],
-    source: "dynamic"
-});
